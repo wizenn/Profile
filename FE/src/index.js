663aa4e95b4736bb5c91fb50f2092f934b0bdda8
wizenn <minhqui2003tg@gmail.com>
2025-05-18 05:52:04 +0700

    .

--- FE/src/index.js
+++ FE/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/home';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
