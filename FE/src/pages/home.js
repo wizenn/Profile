725696f4bd8a15654f7029f3825403b9c321b51b
minhquis <minhqui2003tg@gmail.com>
2025-05-27 19:50:37 +0700

    .

--- FE/src/pages/home.js
+++ FE/src/pages/home.js
@@ -6,15 +6,14 @@ import Navbar from '../components/public-components/navbar';
 
 
 const Home = () => {
-    const { i18n } = useTranslation();
-    console.log('Home render, language:', i18n.language);
+
     return (
         <div className="bg-black h-screen overflow-hidden">
             <div>
                 <Navbar />
             </div>
             <div className="pt-14">
-                <Banner key={i18n.language} />
+                <Banner />
 
             </div>
 
